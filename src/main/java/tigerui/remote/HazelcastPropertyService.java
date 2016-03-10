7f917154e0bf7b4505fd246a2b0269043d0d8c8b
Mike Baum <michael.baum@gmail.com>
2016-03-10 13:40:45 -0500

    Added remote property classes that where removed from https://github.com/Tiger-UI/tigerui-core.git

---
+++ src/main/java/tigerui/remote/HazelcastPropertyService.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2015 Mike Baum
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package tigerui.remote;
+
+import static java.util.Objects.requireNonNull;
+import static tigerui.Preconditions.checkArgument;
+import static tigerui.Preconditions.checkState;
+
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import com.hazelcast.core.HazelcastException;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.core.LifecycleEvent.LifecycleState;
+import com.hazelcast.core.LifecycleService;
+import com.hazelcast.map.listener.EntryUpdatedListener;
+
+import tigerui.Subscriber;
+import tigerui.subscription.CompositeSubscription;
+
+/**
+ * A property service that is backed by Hazelcast.
+ * 
+ * TODO: what about locking and checked exceptions. Right now Hazelcast throws
+ * instances of {@link HazelcastException} which are not checked exceptions.
+ */
+public class HazelcastPropertyService implements PropertyService {
+
+    private static final String PROPERTY_SERVICE_MAP_NAME = "Property";
+    
+    private final IMap<String, Object> propertyMap;
+    private final CompositeSubscription subscriptions;
+    
+    private volatile boolean isRunning;
+
+    public HazelcastPropertyService(HazelcastInstance hazelcast) {
+        this.propertyMap = requireNonNull(hazelcast.getMap(PROPERTY_SERVICE_MAP_NAME));
+        isRunning = hazelcast.getLifecycleService().isRunning();
+        subscriptions = new CompositeSubscription();
+        addLifecycleListener(hazelcast);
+    }
+
+    @Override
+    public <T> T getValue(PropertyId<T> id) {
+        checkPropertyExists(id);
+        checkHazelcastIsRunning();
+        
+        @SuppressWarnings("unchecked") // this should not throw, since the property id guarantees the type
+        T value = (T) propertyMap.get(id.getUuid());
+        
+        return value;
+    }
+
+    @Override
+    public <T> void setValue(PropertyId<T> id, T value) {
+        checkPropertyExists(id);
+        checkHazelcastIsRunning();
+        
+        propertyMap.set(id.getUuid(), value);
+    }
+
+    @Override
+    public <T> void registerProperty(PropertyId<T> id, T initialValue) {
+        checkPropertyDoesNotExists(id);
+        checkHazelcastIsRunning();
+        
+        propertyMap.put(id.getUuid(), initialValue);
+    }
+
+    @Override
+    public Set<PropertyId<?>> getRegisteredProperties() {
+        checkHazelcastIsRunning();
+        
+        return propertyMap.keySet().stream().map(PropertyId::new).collect(Collectors.toSet());
+    }
+    
+    @Override
+    public <T> boolean hasProperty(PropertyId<T> id) {
+        checkHazelcastIsRunning();
+        
+        return propertyMap.containsKey(id.getUuid());
+    }
+
+    @Override
+    public <T> Subscriber registerListener(PropertyId<T> id, Consumer<T> listener) {
+        checkPropertyExists(id);
+        checkHazelcastIsRunning();
+        
+        String registrationId = propertyMap.addEntryListener(createEntryUpdatedListener(listener), id.getUuid(), true);
+        
+        Subscriber subscriber = new Subscriber();
+        subscriptions.add(subscriber);
+        subscriber.doOnDispose(() -> propertyMap.removeEntryListener(registrationId));
+        subscriber.doOnDispose(() -> subscriptions.remove(subscriber));
+        
+        return subscriber;
+    }
+
+    @Override
+    public <T> RemoteProperty<T> getProperty(PropertyId<T> id) {
+        checkHazelcastIsRunning();
+        
+        return RemoteProperty.createRemoteProperty(this, id);
+    }
+
+    private <T> void checkPropertyExists(PropertyId<T> id) {
+        checkArgument(hasProperty(id), "A property with the id: [" + id + "] does not exist.");
+    }
+    
+    private <T> void checkPropertyDoesNotExists(PropertyId<T> id) {
+        checkArgument(!hasProperty(id), "A property with the id: [" + id + "] already exists.");
+    }
+
+    private void checkHazelcastIsRunning() {
+        checkState(isRunning, "Hazelcast is not running");
+    }
+    
+    private <T> EntryUpdatedListener<String, T> createEntryUpdatedListener(Consumer<T> listener) {
+        return event -> listener.accept(event.getValue());
+    }
+    
+    private void addLifecycleListener(HazelcastInstance hazelcast) {
+        LifecycleService lifecycleService = hazelcast.getLifecycleService();
+        
+        lifecycleService.addLifecycleListener(event -> {
+            if (event.getState() == LifecycleState.SHUTDOWN) {
+                isRunning = false;
+                subscriptions.dispose();
+            }
+        });
+    }
+}
---
+++ src/main/java/tigerui/remote/RemotePropertyPublisher.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2015 Mike Baum
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package tigerui.remote;
+
+import static java.util.Objects.requireNonNull;
+import static tigerui.Preconditions.checkState;
+
+import tigerui.EventLoop;
+import tigerui.Subscriber;
+import tigerui.dispatcher.Dispatcher;
+import tigerui.dispatcher.PropertyDispatcher;
+import tigerui.disposables.Disposable;
+import tigerui.property.PropertyObserver;
+import tigerui.property.PropertySubscriber;
+import tigerui.property.publisher.PropertyPublisher;
+import tigerui.subscription.Subscription;
+
+/**
+ * A remote property publisher is connected to a remote property using the
+ * provided {@link PropertyService}. This property will receive updates via
+ * callbacks from a remote source.
+ * 
+ * @param <T> the type of data of the remote property 
+ */
+public class RemotePropertyPublisher<T> implements PropertyPublisher<T>, Disposable {
+    
+    private final PropertyId<T> id;
+    private final PropertyDispatcher<T> dispatcher;
+    private final Subscriber subscriber;
+    private final EventLoop eventLoop;
+    private T value; // always accessed on the EDT does not need to be volatile
+    
+    public RemotePropertyPublisher(PropertyService propertyService, PropertyId<T> id) {
+        this.id = requireNonNull(id);
+        this.dispatcher = Dispatcher.createPropertyDispatcher();
+        this.eventLoop = EventLoop.createEventLoop();
+        this.value = requireNonNull(propertyService.getValue(id));
+        this.subscriber = propertyService.registerListener(id, this::updateValue);
+        subscriber.doOnDispose(this::dispose);
+    }
+
+    @Override
+    public T get() {
+        return value;
+    }
+    
+    @Override
+    public Subscription subscribe(PropertyObserver<T> observer) {
+        PropertySubscriber<T> subscriber = dispatcher.subscribe(observer);
+        
+        // push the latest value to the subscriber
+        subscriber.onChanged(get());
+
+        // dispose if this property is already disposed
+        if (dispatcher.isDisposed())
+            subscriber.onDisposed();
+        
+        return subscriber;
+    }
+
+    @Override
+    public void dispose() {
+        eventLoop.checkInEventLoop();
+        subscriber.dispose();
+        dispatcher.dispose();
+    }
+    
+    public PropertyId<T> getId() {
+        return id;
+    }
+    
+    private void updateValue(T newValue) {
+        checkState(!eventLoop.isInEventLoop(), "Update should not be called from the event loop");
+        
+        eventLoop.invokeLater(() -> {
+            value = newValue;
+            dispatcher.dispatch(newValue);
+        });
+    }
+}
